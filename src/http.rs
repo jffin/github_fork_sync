39cdbfe17cbb8674aecd12f1132f9c9b3ad83901
jff <22579497+jffin@users.noreply.github.com>
2020-11-10 22:52:59 +0200

    some improvements 0.1.3

--- src/http.rs
+++ src/http.rs
@@ -7,7 +7,8 @@ const REQUEST_URL: &str = "https://api.github.com/repos";
 const AUTH_HEADER: &str = "Authorization";
 const ACCEPT_HEADER: &str = "Accept";
 const ACCEPT_HEADER_VALUE: &str = "application/vnd.github.v3+json";
-const SAFARI_USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36";
+const SAFARI_USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
+(KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36";
 
 
 #[derive(Deserialize)]
